3c16756e0876a06ea91780942a9f3ecbca16e8cc
Fullonikon <maxim-199910@mail.ru>
2020-02-19 15:52:12 +0300

    214124

--- Test CSharp/Form1.Designer.cs
+++ Test CSharp/Form1.Designer.cs
@@ -32,24 +32,30 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(28, 34);
-            this.pictureBox1.MaximumSize = new System.Drawing.Size(432, 262);
+            this.pictureBox1.Location = new System.Drawing.Point(19, 22);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(288, 170);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(321, 200);
+            this.pictureBox1.Size = new System.Drawing.Size(214, 130);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(625, 207);
+            this.button1.Location = new System.Drawing.Point(417, 135);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 89);
+            this.button1.Size = new System.Drawing.Size(103, 58);
             this.button1.TabIndex = 1;
             this.button1.Text = "батон хлеба";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,34 +63,76 @@
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(28, 271);
-            this.pictureBox2.MaximumSize = new System.Drawing.Size(432, 262);
+            this.pictureBox2.Location = new System.Drawing.Point(19, 176);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox2.MaximumSize = new System.Drawing.Size(288, 170);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(321, 198);
+            this.pictureBox2.Size = new System.Drawing.Size(214, 129);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(387, 377);
+            this.label1.Location = new System.Drawing.Point(258, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(390, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 51);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "увеличить яркость батона";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(317, 245);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 20);
+            this.numericUpDown1.TabIndex = 7;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1031, 510);
+            this.ClientSize = new System.Drawing.Size(687, 331);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "окошко";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -96,6 +144,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- Test CSharp/Form1.cs
+++ Test CSharp/Form1.cs
@@ -18,35 +18,73 @@ namespace Test_CSharp
 
         private Bitmap image;
         private Bitmap invimage;
-        
+
+        public Int32 Clamp(Int32 a, Int32 min, Int32 max)
+        {
+            Int32 res = a;
+            if (a < min) res = min;
+            else if (a > max) res = max;
+            return res;
+        }
+
         // инверсия цвета       я хз почему так функция пишется
         public Color invert(Color c) => Color.FromArgb(c.A, 0xFF - c.R, 0xFF - c.G, 0xFF - c.B);
+        // полутон, в скобках чёрно белый фильтр
         public Color poluton(Color c) => Color.FromArgb(c.A, (Int32)(0.299 * c.R + 0.587 * c.G + 0.114 * c.B), (Int32)(0.299 * c.R + 0.587 * c.G + 0.114 * c.B), (Int32)(0.299 * c.R + 0.587 * c.G + 0.114 * c.B));
+        // сепия
         public Color sepia(Color c)
         {
             Int32 intensity = (Int32)(0.299 * c.R + 0.587 * c.G + 0.114 * c.B);
-            int k = Math.Clamp(4, 1, 3);
-            Int32 R = (Int32)((0.299 * c.R + 0.587 * c.G + 0.114 * c.B) * k);
-            return Color.FromArgb(c.A, 0xFF - c.R, 0xFF - c.G, 0xFF - c.B);
+            double k = 20;
+            Int32 R = Clamp((Int32)(intensity + 2 * k), 0, 255);
+            Int32 G = Clamp((Int32)(intensity + 0.5 * k), 0, 255);
+            Int32 B = Clamp((Int32)(intensity - 1 * k), 0, 255);
+            return Color.FromArgb(c.A, R, G, B);
         }
-        private void button1_Click(object sender, EventArgs e)
+        // увеличить яркость // работает по дэбильному
+        public Color increse(Color c, int inc)
         {
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; 
-            image = new Bitmap(@"E:\Cats\cat-art-images-wallpaper.jpg"); // полный путь до картинки
+            return Color.FromArgb(c.A, Clamp(c.R + inc, 0, 255), Clamp(c.G + inc, 0, 255), Clamp(c.B + inc, 0, 255));
+        }
+        private void button1_Click(object sender, EventArgs e) // главная кнопочка. батон хлеба
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            image = new Bitmap(@".\267464-werecat.jpg"); // полный путь до картинки
             pictureBox1.Image = image;
-            invimage = new Bitmap(@"E:\Cats\cat-art-images-wallpaper.jpg"); // собака перед путём обязательно
+            invimage = new Bitmap(@".\267464-werecat.jpg"); // собака перед путём обязательно
             int imax = image.Width;
             int jmax = image.Height;
             for (int j = 0; j < jmax; j++)
             {
                 for (int i = 0; i < imax; i++)
                 {
-                    invimage.SetPixel(i, j, poluton(invimage.GetPixel(i, j)));
+                    invimage.SetPixel(i, j, sepia(invimage.GetPixel(i, j))); // сюда вставлять фильтры
                 }
             }
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.Image = invimage;
             label1.Text = "сюда лут";
+            label2.Text = "отсюда гуд";
+        }
+
+        private void button2_Click(object sender, EventArgs e) // яркость
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            image = new Bitmap(@".\267464-werecat.jpg"); // полный путь до картинки
+            pictureBox1.Image = image;
+            invimage = new Bitmap(@".\267464-werecat.jpg"); // собака перед путём обязательно
+            int imax = image.Width;
+            int jmax = image.Height;
+            int inc = (int)numericUpDown1.Value;
+            for (int j = 0; j < jmax; j++)
+            {
+                for (int i = 0; i < imax; i++)
+                {
+                    invimage.SetPixel(i, j, increse(invimage.GetPixel(i, j), inc));
+                }
+            }
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = invimage;
         }
     }
 }
